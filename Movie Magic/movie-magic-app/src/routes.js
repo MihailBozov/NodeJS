a4a06aaa6eb0a9849c19d330891284d166a95c32
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-09-26 19:36:30 +0300

    Add create movie page

--- Movie Magic/movie-magic-app/src/routes.js
+++ Movie Magic/movie-magic-app/src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import homeController from './controllers/homeController.js';
+import movieController from './controllers/movieController.js'
 
 const router = Router();
 
 
 router.use(homeController);
 
+// here our movieController will be used only if the route starts with 'movies' 
+router.use('/movies', movieController);
 
 export default router;
