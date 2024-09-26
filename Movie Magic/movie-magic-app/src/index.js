177d0557a6dffac62698e320608a0f29fc14ed43
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-09-26 13:21:22 +0300

    Render first view

--- Movie Magic/movie-magic-app/src/index.js
+++ Movie Magic/movie-magic-app/src/index.js
@@ -1,6 +1,12 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+import express from 'express';
+import handlebars from 'express-handlebars';
+import path from 'path';
+
+
+// const express = require('express');
+// const handlebars = require('express-handlebars');
+// const path = require('path');
+
 
 const app = express();
 const port = 5000;
@@ -8,10 +14,10 @@ const port = 5000;
 app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views')); 
+app.set('views', path.join('src', 'views')); 
 
 app.use(express.static('public'))
-
+ 
 app.get('/', (req, res) => {
     res.render('home', {'layout': false})
 });
