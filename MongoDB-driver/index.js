97004421d16259f38386010bd09d3c02b8257a89
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-09-28 17:33:05 +0300

    Set up connection to the database

---
+++ MongoDB-driver/index.js
@@ -0,0 +1,17 @@
+import { MongoClient } from 'mongodb';
+
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'myProject';
+
+async function main() {
+    await client.connect();
+    console.log('Connected Successfully to the server');
+    const db = client.db(dbName);
+    const collection = db.collection('documents');
+    
+    return 'done!';
+}
+
+main().then(console.log).catch(console.error).finally(() => client.close())
