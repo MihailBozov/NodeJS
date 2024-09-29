1e5ad3a32135f3d378c386e527f05e31f3ffc77a
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-09-29 14:50:40 +0300

    Get student form the db

--- mongoose-demo/index.js
+++ mongoose-demo/index.js
@@ -17,34 +17,44 @@ main()
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Hello from express');
 })
 
-app.get('/students', async (req, res) => {
-    
-    const students = await Student.find({},{name: true, age:true, _id: false}).lean();
-    res.json(students);
-})
-
 app.get('/students/create', (req, res) => {
     res.send(`
-    <form method="post">
+        <form method="post">
         <input type="text" name="name" placeholder="Name">
         <input type="number" name="age" placeholder="Age">
         <input type="submit" value="create">
-    </form>
+        </form>
         `);
 })
 
+// Read from a db
+app.get('/students', async (req, res) => {
+    const students = await Student.find().lean();
+    res.json(students);
+})
+
+
+// Create new record in a db
 app.post('/students/create', async (req, res) => {
     const student = await Student.create(req.body);
-    
+
     console.log(student);
     res.redirect('/students')
 })
 
+//update record in a db
+
+app.get('/students/:id/update', async (req, res) => {
+    const student = await Student.findById(req.params.id);
+    // const student = await Student.find({_id: req.params.id})
+    
+    console.log(student);
+})
 
 app.listen(5000, () => console.log('Listening on port 5000'));
