6826fe4258b32ddbca9d70b30fb727af3ffe01f0
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-10-01 11:39:30 +0300

    Create Movie model (--continuing with the workshop--)

---
+++ Movie Magic/movie-magic-app/src/models/Movie.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+    tittle: String,
+    genre: String,
+    director: String,
+    year: Number,
+    rating: Number,
+    description: String,
+    img: String,
+});
+
+ const Movie = mongoose.model('Movie', movieSchema);
+
+ export default Movie;
