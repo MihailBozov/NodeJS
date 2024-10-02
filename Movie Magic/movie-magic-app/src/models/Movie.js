f3177ee21744d5dd2d706b04e0b338d87ce5ca85
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-10-02 16:51:14 +0300

    Some validations made

--- Movie Magic/movie-magic-app/src/models/Movie.js
+++ Movie Magic/movie-magic-app/src/models/Movie.js
@@ -1,13 +1,38 @@
 import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema({
-    tittle: String,
-    genre: String,
-    director: String,
-    year: String,
-    rating: Number,
-    description: String,
-    img: String,
+    tittle: {
+        type: String, 
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: String, 
+        required: true,
+        min: 1900,
+        max: 2025,
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 1000,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
     casts: [{
         type: mongoose.Types.ObjectId,
         ref: 'Cast'
