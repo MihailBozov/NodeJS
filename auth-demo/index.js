2f8c5a031e711fae5e005575e2ec6c72f66b599b
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-10-03 15:17:16 +0300

    Set up basic template

--- auth-demo/index.js
+++ auth-demo/index.js
@@ -1,1 +1,19 @@
 import express from 'express';
+
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send(`
+                <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a>
+                <p>Home Page works!</p>
+        `)
+})
+
+app.get('/login', (req, res) => {
+    res.send(`
+                <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a>
+                <p>Login Page works!</p>
+        `)
+})
+
+app.listen('5000', () => 'The server is listening on port 5000...')
