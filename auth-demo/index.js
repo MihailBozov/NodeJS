86fbbec34c3375b878a4567279bdcdf819549233
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-10-04 15:25:44 +0300

    Saving all the types (e.g. npm i --save-dev @types/bcrypt)

--- auth-demo/index.js
+++ auth-demo/index.js
@@ -8,7 +8,7 @@ app.use(session({
     secret: 'my secret',
     resave: false,
     saveUninitialized: true,
-    cookie: {sequre: true}
+    cookie: { sequre: true }
 }))
 
 app.get('/', (req, res) => {
@@ -19,16 +19,38 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-
     req.session.hello = Date.now();
-    
-    res.send(`
-            <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a>
-            <p>Login Page works!</p>
-        `);
-        
-    res.end();
 
+    res.send(`
+            <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a><br><br>
+            
+            <form action="/login" method="post">
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div>
+                    <br>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        `);
+
+    // res.send(`
+    //         <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a>
+    //         <p>Login Page works!</p>
+    //     `);
+
+    res.end();
+});
+
+app.post('/login', (req, res) => {
+    
+    console.log(req.body);
 })
 
 app.listen('5000', () => 'The server is listening on port 5000...')
