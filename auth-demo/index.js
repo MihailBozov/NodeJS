68e5277a3cf53ed1b2b0e2386bdac99af7248d79
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-10-04 19:44:05 +0300

    Login logic, comparing passwords

--- auth-demo/index.js
+++ auth-demo/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send(`
             <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a><span style="white-space: pre">   </span><a href="/register">Register</a><br><br>
-            <p>Home Page works!</p>
+            <h2>Home Page works!</h2>
         `)
 })
 
@@ -24,7 +24,7 @@ app.get('/register', (req, res) => {
 
     res.send(`
             <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a><span style="white-space: pre">   </span><a href="/register">Register</a><br><br>
-            
+            <h2>Register</h2>
             <form action="/register" method="post">
                 <div>
                     <label for="email">Email</label>
@@ -43,24 +43,66 @@ app.get('/register', (req, res) => {
     res.end();
 });
 
-
 const registeredUsers = {};
 
-
 app.post('/register', async (req, res) => {
+    
+    const { email, password } = req.body;
 
     const salt = await bcrypt.genSalt(10);
-
-    const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, salt);
-
-    console.log('Plain password :', password);
-    console.log('Salt           :', salt);
-    console.log('Hashed password:', hashedPassword);
     
-    registeredUsers[username] = hashedPassword;
-
+    registeredUsers[email] = hashedPassword;
+    
     res.redirect('/login')
+})
+
+app.get('/login', (req, res) => {
+        
+    res.send(`
+            <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a><span style="white-space: pre">   </span><a href="/register">Register</a><br><br>
+            <h2>Login</h2>
+            <form action="/login" method="post">
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div>
+                    <br>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        `)
+        res.end();
+})
+
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    
+    if(!registeredUsers[email]) {
+       return res.status(400).end();
+    }
+    
+    const plainPassword = password;
+    const hashedPassword =registeredUsers[email];
+    
+    const compared = await bcrypt.compare(password, hashedPassword)
+    
+    if(compared) {
+        return res.send(`
+            <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a><span style="white-space: pre">   </span><a href="/register">Register</a><br><br>
+            <h2>Welcome</h2>
+            `).end()
+    } else {
+        return res.send(`
+            <span style="white-space: pre">      </span><a href="/">Home<\a><span style="white-space: pre">   </span><a href="/login">Login</a><span style="white-space: pre">   </span><a href="/register">Register</a><br><br>
+            <h2>Invalid email or password</h2>
+            `).end()
+    }
 })
 
 app.listen('5000', () => 'The server is listening on port 5000...')
