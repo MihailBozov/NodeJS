9a5b470ac153f5a00fd936c2bcc6807f57aed2cb
MihailBozov <129510773+MihailBozov@users.noreply.github.com>
2024-10-06 13:47:33 +0300

    Hosting data on Mongo Atlas and using environmental variables (dotenv)

--- Movie Magic/movie-magic-app/src/config/mongooseConfig.js
+++ Movie Magic/movie-magic-app/src/config/mongooseConfig.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-const dbUrl = 'mongodb://localhost:27017/magic-movies';
+dotenv.config();
+
+const dbUrl = process.env.DB_ATLASS_URL || process.env.DB_LOCAL_URL;
 
 export default async function mongooseConfig() {
     try {
+        console.log('DB_ATLASS_URL:', process.env.DB_ATLASS_URL)
+        console.log('DB_LOCAL_URL:', process.env.DB_LOCAL_URL)
+        
         await mongoose.connect(dbUrl);
-        console.log('Connected!')
+        console.log('Connected to db:', dbUrl);
     } catch(error) {
         console.error('Error:', error);
     }
